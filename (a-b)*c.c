e1a7e84b693c7559107f090f64207e25a161fbaf
Yash Kumar Rai <Yash.28323@stu.upes.ac.in>
2025-08-13 22:39:53 +0530

    Add files via upload

---
+++ (a-b)*c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2,num3,s;
+    printf("Enter three Number");
+    scanf("%d,%d,%d,&5,&3,&2");
+    s=(num1-num2)*num3;
+    printf("Answer of the operation is %d",num1,num2,num3,s);
+    return 0;
+}
---
+++ a*b.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2,s;
+    printf("Enter 2 NUmber");
+    scanf("%d,%d,&a,&b");
+    s=num1*num2;
+    printf("Multiplication of %d and %d is %d", num1,num2,s);
+    return 0;
+}
