b55130b76d53d0d45366bb1c12a07d7c8d57ee98
Yash Kumar Rai <Yash.28323@stu.upes.ac.in>
2025-08-13 21:49:46 +0530

    Add files via upload

---
+++ a-b+c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2,num3,s;
+    printf("Enter three Number");
+    scanf("%d,%d,%d,&a,&b,&c");
+    s=num1-num2+num3;
+    printf("sum of %d,%d,%d is %d",num1,num2,num3,s);
+    return 0;
+}
