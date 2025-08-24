03e2f0833ac50c75a6b29363898eaa579277c82f
Yash Kumar Rai <Yash.28323@stu.upes.ac.in>
2025-08-24 10:45:56 +0530

    Update and rename (a-b)*c.c to a-(b*c).c

---
+++ a-(b*c).c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c,x;
+    printf("Enter three Number");
+    scanf("%d,%d,%d",&a,&b,&c);
+    x=a-(b*c);
+    printf(" Answer of the operation is %d",x);
+    return 0;
+}
