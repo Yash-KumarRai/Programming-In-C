c4ed42f0253ab5bd45df503284bfac113a795719
Yash Kumar Rai <Yash.28323@stu.upes.ac.in>
2025-08-24 10:26:52 +0530

    Update a*b.c

--- a*b.c
+++ a*b.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    int num1,num2,s;
+    int a,b,p;
     printf("Enter 2 NUmber");
-    scanf("%d,%d,&a,&b");
-    s=num1*num2;
-    printf("Multiplication of %d and %d is %d", num1,num2,s);
+    scanf("%d,%d",&a,&b);
+    s=a*b;
+    printf("Multiplication of %d and %d is %d", a,b,p);
     return 0;
 }
